a43c6b0dccb11565b68c4c648c027669595dad1f
TP042126 <TP042126@mail.apu.edu.my>
2019-11-12 17:26:36 +0800

    Added Toby

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace Tutorial2.Controllers
             return View();
         }
 
+        public IActionResult Toby()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
